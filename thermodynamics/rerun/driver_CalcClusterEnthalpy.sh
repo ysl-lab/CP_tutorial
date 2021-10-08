c9498676c6c1a48af8b41b35b854b456f23c7478
jovan-damjanovic <53581211+jovan-damjanovic@users.noreply.github.com>
2021-10-08 08:04:02 -0400

    Delete thermodynamics directory

--- thermodynamics/rerun/driver_CalcClusterEnthalpy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-
-source include.sh
-
-
-for ((ic=$IC; ic<=$NC; ic++ )); do
-  echo "Calculating thermodynamics for cluster $ic ..."
-  python Py_CalcClusterEnthalpy.py ENERGY_complex_cluster_$ic.dat ENERGY_solute_cluster_$ic.dat ENERGY_solvent_cluster_$ic.dat > ENTHALPY_cluster_$ic.dat
-done
--- thermodynamics/thermodynamics/top3_v6/Py_GenTableGlobalVar.py
+++
@@ -1,5 +0,0 @@
-IC=3
-NC=5
-vmin=-8
-vmax=8
-vstep=4
