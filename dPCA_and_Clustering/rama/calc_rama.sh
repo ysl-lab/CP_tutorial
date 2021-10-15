123cb153b09d3e1cf8dc7a0b1406ee89e5bd9f30
jovan-damjanovic <53581211+jovan-damjanovic@users.noreply.github.com>
2021-10-15 06:57:07 -0400

    Update calc_rama.sh

--- dPCA_and_Clustering/rama/calc_rama.sh
+++ dPCA_and_Clustering/rama/calc_rama.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
+#Change PROT below to match your system
+#NS decides number of states to be plotted
 PROT=cTEMPPROT
-NSs1=S1NUMCLUSTERS
-NSs2=S2NUMCLUSTERS
+NS=5
 
-for ((i=0; i<=$NSs1; i++)); do
-   python calcFreeEnergy_6mers_v2.py s1${PROT}_phipsi/cluster$i.txt s1${PROT}_cluster$i.png
-done
-
-for ((i=0; i<=$NSs2; i++)); do
-   python calcFreeEnergy_6mers_v2.py s2${PROT}_phipsi/cluster$i.txt s2${PROT}_cluster$i.png
+for ((i=1; i<=$NS; i++)); do
+   echo 'Calculating cluster' ${i}
+   python calcFreeEnergy_v2_5mers.py s1${PROT}_phipsi/cluster$i.txt s1${PROT}_cluster$i.png
+   python calcFreeEnergy_v2_5mers.py s2${PROT}_phipsi/cluster$i.txt s2${PROT}_cluster$i.png
 done
