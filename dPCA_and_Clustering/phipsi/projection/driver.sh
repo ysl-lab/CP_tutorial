0f8d8bc9d09f88598353a766a47df9afeafece5c
jovan-damjanovic <53581211+jovan-damjanovic@users.noreply.github.com>
2021-10-15 06:04:03 -0400

    Update driver.sh

--- dPCA_and_Clustering/phipsi/projection/driver.sh
+++ dPCA_and_Clustering/phipsi/projection/driver.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
+#Change PROT to match your system
 PROT=cTEMPPROT
 
 function combine(){
    local fname=$1
    inp1=pc1_pc2_pc3/${fname}_1.xvg
    inp2=pc1_pc2_pc3/${fname}_2.xvg
-
-   tail -n +13 $inp1 > tmp1 
-   tail -n +13 $inp2 > tmp2
+#Removes xmgrace headers from the .xvg files
+   tail -n +18 $inp1 > tmp1 
+   tail -n +18 $inp2 > tmp2
 
    echo "   Processing $inp1 $inp2 ..."
    python Py_combine.py tmp1 tmp2 > pc1_pc2_pc3/$fname.txt
