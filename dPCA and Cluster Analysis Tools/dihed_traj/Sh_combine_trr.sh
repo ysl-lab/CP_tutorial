7a62bec9b3fdb0c9b6ff68916a35ed26fba3ce5a
jovan-damjanovic <53581211+jovan-damjanovic@users.noreply.github.com>
2023-01-17 14:06:46 -0500

    Update Sh_combine_trr.sh

--- dPCA and Cluster Analysis Tools/dihed_traj/Sh_combine_trr.sh
+++ dPCA and Cluster Analysis Tools/dihed_traj/Sh_combine_trr.sh
@@ -9,7 +9,7 @@ for i in ${traj[@]}; do
    cd ${i}${PROT}
       #trjcat_mpi -f ${i}${PROT}_10.trr ${i}${PROT}_11.trr ${i}${PROT}_12.trr ${i}${PROT}_13.trr ${i}${PROT}_14.trr -cat -o ../${i}${PROT}_all.trr &> ${i}.txt
       #trjcat_mpi -f ${i}${PROT}_11.trr ${i}${PROT}_12.trr ${i}${PROT}_13.trr ${i}${PROT}_14.trr ${i}${PROT}_15.trr -cat -o ../${i}${PROT}_all.trr
-      gmx_mpi trjcat -f ${i}${PROT}_12.trr ${i}${PROT}_13.trr ${i}${PROT}_14.trr ${i}${PROT}_15.trr ${i}${PROT}_16.trr -cat -o ../${i}${PROT}_all.trr &> ${i}.txt
+      gmx_mpi trjcat -f ${i}${PROT}_12.trr ${i}${PROT}_13.trr ${i}${PROT}_14.trr ${i}${PROT}_15.trr ${i}${PROT}_16.trr -cat -nosort -o ../${i}${PROT}_all.trr &> ${i}.txt
       #trjcat_mpi -f ${i}${PROT}_13.trr ${i}${PROT}_14.trr ${i}${PROT}_15.trr ${i}${PROT}_16.trr ${i}${PROT}_17.trr -cat -o ../${i}${PROT}_all.trr
       #trjcat_mpi -f ${i}${PROT}_14.trr ${i}${PROT}_15.trr ${i}${PROT}_16.trr ${i}${PROT}_17.trr ${i}${PROT}_18.trr -cat -o ../${i}${PROT}_all.trr
    cd ..
